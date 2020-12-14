aa61ee16df2859bfb3ece12b1deaeaa98edccc3a
Cloud User <ec2-user@ip-10-0-0-215.us-west-2.compute.internal>
2020-12-14 05:00:27 +0000

    run.sh

--- timestream/group_by_measure_name/run.sh
+++ timestream/group_by_measure_name/run.sh
@@ -1,6 +1,7 @@
 # python manage.py
 # sleep 5
 
+# Single thread, batch 1, 100, 101, no grouping by measurename
 # python ingest.py --database-name TestDB1 --table-name BatchTbl1 --endpoint us-west-2 -c 1 -b 1 & 
 # MY_PID=$!
 # sleep 10
@@ -19,6 +20,8 @@ sleep 5
 # sudo kill -SIGINT $MY_PID
 # sleep 5
 
+
+# Single thread, batch 1, 100, 101, grouping by measurename
 
 # python ingest.py --database-name TestDB1 --table-name BatchTbl1 --endpoint us-west-2 -c 1 -b 1 -g True & 
 # MY_PID=$!
